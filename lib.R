716feb4856a886f75d0fcbb4495a81d310f05afa
Gigi SR <georgina.roughsedge@gmx.com>
2017-09-29 23:13:26 +0100

    Have added further distributions to the numeric_values function

--- lib.R
+++ lib.R
@@ -2,36 +2,71 @@
 # Functions #
 #############
 
-# Need a function work out the cumulative odds bit of the table
 # Need functions for up to 4 levels of cateogry variables
-# Need functions for assigning numeric values over a distribution
 
 # Do we need facility for manual overrides for certain criteria?
 
 # Add in a default value for level 2+
 
+# calc_cumulative
+
+    # INPUTS
+    #   df    - the odds table to calculate the cumulative values for
+
+    # OUTPUT
+    #   output - the transformed table
+
+calc_cumulative <- function( df ) {
+    
+    level <- df %>%
+        select( starts_with( 'filter' ) ) %>% 
+        dim %>%
+        .[2]
+    
+    print( level ) 
+    
+    if ( level == 0 ) {
+        
+        output <- df %>% 
+            mutate( cumulative_odds = cumsum( odds ) )
+        
+    } else {
+        
+        filter_names <- paste0( 'filter_', seq( 1, level ) )
+        
+        output <- df %>% 
+            group_by_( .dots = filter_names ) %>% 
+            mutate( cumulative_odds = cumsum( odds ) ) %>% 
+            ungroup
+        
+    } 
+    
+    return( output )
+    
+}
+
 # level_1_values_function
 
-  # INPUTS
-  #   tbl - this is a table containing the desired output values as well
-  #         as the probabilies
-  #   n   - this is the size of output deisred
-  
-  # OUTPUT
-  #   output - this will be a vector
+    # INPUTS
+    #   tbl - this is a table containing the desired output values as well
+    #         as the probabilies
+    #   n   - this is the size of output deisred
+    
+    # OUTPUT
+    #   output - this will be a vector
 
 level_1_values_function <- function ( tbl, n ) {
   
-  random_values <- runif( n )
-  
-  output <- cut(
-    x      = random_values,
-    breaks = c( 0, tbl %>% select( cumulative_odds ) %>% .[[1]] ),
-    labels = tbl %>% select( out_value ) %>% .[[1]]
-  ) %>% 
-    as.character
-  
-  return( output )
+    random_values <- runif( n )
+
+    output <- cut(
+        x      = random_values,
+        breaks = c( 0, tbl %>% select( cumulative_odds ) %>% .[[1]] ),
+        labels = tbl %>% select( out_value ) %>% .[[1]]
+    ) %>% 
+        as.character
+
+    return( output )
   
 }
 
@@ -65,16 +100,16 @@ level_1_adj_values_function <- function (
 
 # level_2_values_function
 
-  # INPUTS
-  #   tbl - this is a table containing the desired output values as well
-  #         as the probabilities
-  #   filter_value - this is the value that the table will be filtered 
-  #         by, the column that will be filtered is filter_1
-  #   n - the number of values to create
-  #   default - is an argument with a default value
-
-  # OUTPUT
-  #   output - outputs a vector of values
+    # INPUTS
+    #   tbl - this is a table containing the desired output values as well
+    #         as the probabilities
+    #   filter_value - this is the value that the table will be filtered 
+    #         by, the column that will be filtered is filter_1
+    #   n - the number of values to create
+    #   default - is an argument with a default value
+    
+    # OUTPUT
+    #   output - outputs a vector of values
 
 level_2_values_function <- function ( 
     odds_tbl, filter_value, n, 
@@ -108,10 +143,10 @@ level_2_values_function <- function (
     }
   
     output <- cut(
-            x      = random_values,
-            breaks = c( 0, tbl %>% select( cumulative_odds ) %>% .[[1]] ),
-            labels = tbl %>% select( out_value ) %>% .[[1]]
-        ) %>% 
+        x      = random_values,
+        breaks = c( 0, tbl %>% select( cumulative_odds ) %>% .[[1]] ),
+        labels = tbl %>% select( out_value ) %>% .[[1]]
+    ) %>% 
         as.character
   
     return( output )
@@ -120,17 +155,17 @@ level_2_values_function <- function (
 
 # level_3_values_function
 
-  # INPUTS
-  #   tbl - this is a table containing the desired output values as well
-  #         as the probabilities
-  #   filter_value_1 - this is the value that the table will be filtered 
-  #         by, the column that will be filtered is filter_1
-  #   filter_value_2 - this is the value that the table will be filtered
-  #         by, the column that will be filtered is filter_2
-  #   n - the number of values to create
-  
-  # OUTPUT
-  #   output - outputs a vector of values
+    # INPUTS
+    #   tbl - this is a table containing the desired output values as well
+    #         as the probabilities
+    #   filter_value_1 - this is the value that the table will be filtered 
+    #         by, the column that will be filtered is filter_1
+    #   filter_value_2 - this is the value that the table will be filtered
+    #         by, the column that will be filtered is filter_2
+    #   n - the number of values to create
+    
+    # OUTPUT
+    #   output - outputs a vector of values
 
 level_3_values_function <- function ( 
         tbl, filter_value_1, filter_value_2, n, 
@@ -145,40 +180,70 @@ level_3_values_function <- function (
           filter_2 == filter_value_2[1]
         )
   
-  if ( nrow( tbl ) == 0 ) {
+    if ( nrow( tbl ) == 0 ) {
     
-    output <- replicate( n, default )
+        output <- replicate( n, default )
+        
+        return( output )
+    
+    }
+    
+    output <- cut(
+        x      = random_values,
+        breaks = c( 0, tbl %>% select( cumulative_odds ) %>% .[[1]] ),
+        labels = tbl %>% select( out_value ) %>% .[[1]]
+    ) %>% 
+        as.character
     
     return( output )
-    
-  }
-  
-  output <- cut(
-    x      = random_values,
-    breaks = c( 0, tbl %>% select( cumulative_odds ) %>% .[[1]] ),
-    labels = tbl %>% select( out_value ) %>% .[[1]]
-  ) %>% 
-    as.character
-  
-  return( output )
   
 }
 
-# numeric_values_function 
+# numeric_values
 
-numeric_values_function <- function ( 
+    # INPUTS
+    #   n           - the number of values to be generated
+    #   type        - the type of generating distribution, takes values:
+    #       "chisq", "unif", "gamma", "binom", "norm"
+    #   df          - the degrees of freedom for the chisq distribution
+    #   min, max    - the lower and upper limits of the distribution
+    #   shape, rate - the parameters of the gamma distribution
+    #   size, prob  - the number of trials, and the probability of success
+    #   mean, sd    - the mean and standard deviation for the normal 
+    #       distribution
+    #   multiplier  - a numeric value to multiply the generated numbers by
+    #   round       - the number of decimal places to round to
+    #   minimum     - a minimum value to cap the numbers at
+    #   maximum     - a maximum value to cap the numbers at
+    
+    # OUTPUT
+    #   . - outputs a vector of values
+
+numeric_values <- function ( 
     n, type, 
     df = 4, 
     min = 0, max = 1,
     shape = 2, rate = 4,
-    multiplier = 1, round = 0
+    size = 1, prob = 0.5,
+    mean = 0, sd = 1,
+    multiplier = 1, round = 0,
+    minimum = -Inf, maximum = Inf
 ) {
     
     temp_func <- function ( x ) {
-        round( x * multiplier, round )
+        
+        val <- x %>% 
+            lapply( function( x ) prod( x, multiplier ) ) %>% 
+            lapply( function( x ) max( x, minimum ) ) %>%
+            lapply( function( x ) min( x, maximum ) ) %>% 
+            unlist %>% 
+            round( round )
+        
+        return( val )
+        
     }
     
-    if ( type == "chi2" ) {
+    if ( type == "chisq" ) {
         return( temp_func( rchisq( n, df ) ) )
     }
     
@@ -188,6 +253,14 @@ numeric_values_function <- function (
     
     if ( type == "gamma" ) {
         return( temp_func( rgamma( n, shape, rate ) ) )
+    }
+    
+    if ( type == "binom" ) {
+        return( temp_func( rbinom( n, size, prob ) ) )
+    }
+    
+    if ( type == "norm" ) {
+        return( temp_func( rnorm( n, mean, sd ) ) )
     }
     
 }
